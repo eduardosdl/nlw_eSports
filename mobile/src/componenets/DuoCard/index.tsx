2095b6117897965309e69a514a588f866f505fdd
eduardosdl <edu.silva.e08@gmail.com>
2023-04-19 18:13:56 -0300

    use send props to duo info component and create button

--- mobile/src/componenets/DuoCard/index.tsx
+++ mobile/src/componenets/DuoCard/index.tsx
@@ -1,18 +1,55 @@
-import { View } from 'react-native/types';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { GameController } from 'phosphor-react-native';
 
 import { DuoInfo } from '../DuoInfo';
 
 import { styles } from './styles';
+import { THEME } from '../../theme';
 
-interface DuoCardProps {}
+export interface DuoCardProps {
+  id: string;
+  hourEnd: string;
+  hourStart: string;
+  name: string;
+  useVoiceChannel: boolean;
+  weekDays: string[];
+  yearsPlaying: number;
+}
 
-export function DuoCard() {
+interface Props {
+  data: DuoCardProps;
+  onConnect: () => void;
+}
+
+export function DuoCard({ data, onConnect }: Props) {
   return (
     <View style={styles.container}>
-      <DuoInfo label="Nome" value="Eduardo Silva" />
-      <DuoInfo label="Nome" value="Eduardo Silva" />
-      <DuoInfo label="Nome" value="Eduardo Silva" />
-      <DuoInfo label="Nome" value="Eduardo Silva" />
+      <DuoInfo label="Nome" value={data.name} />
+
+      <DuoInfo
+        label="Tempo de jogo"
+        value={`${data.yearsPlaying} ${
+          data.yearsPlaying == 1 ? 'ano' : 'anos'
+        }`}
+      />
+
+      <DuoInfo
+        label="Disponibilidade"
+        value={`${data.weekDays.length} dias \u2022 ${data.hourStart} - ${data.hourEnd}`}
+      />
+
+      <DuoInfo
+        label="Chamada de áudio"
+        value={data.useVoiceChannel ? 'Sim' : 'Não'}
+        colorValue={
+          data.useVoiceChannel ? THEME.COLORS.SUCCESS : THEME.COLORS.ALERT
+        }
+      />
+
+      <TouchableOpacity style={styles.button} onPress={onConnect}>
+        <GameController color={THEME.COLORS.TEXT} size={20} />
+        <Text style={styles.buttonTitle}>Connectar</Text>
+      </TouchableOpacity>
     </View>
   );
 }
